db400343f3c623e26d96ebc6ce200144c78825eb
Sweta Dahal <dahalsweta5@gmail.com>
2026-07-14 11:40:57 +0545

    feat: setup tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-indigo-600">
+        LMS Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
